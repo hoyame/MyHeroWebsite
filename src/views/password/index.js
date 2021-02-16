aacc26e20ab80dca92744ae8c3e5f437369f526f
hoyame <zou.hoyame@gmail.com>
2021-02-16 15:13:54 +0100

    view: init password forget

---
+++ src/views/password/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const PasswordView = () => {
+    return (
+        <>
+            <p>pass</p>
+        </>
+    );
+}
+
+export default PasswordView;
