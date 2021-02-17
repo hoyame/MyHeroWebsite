ad7906effa1c82be2e0772ff1935df64adf419c8
hoyame <zou.hoyame@gmail.com>
2021-02-17 12:27:47 +0100

    feat: add logo and copyright

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+        <img className="logo" src="https://cdn.discordapp.com/attachments/650778484523794456/789992516505305098/Sans_titre_-_1.jpg"></img>
+        <p className="text-def copyright">© 2020-2021 MyHeroes. Tous droits réservés. </p>
         <Router>
           <Switch>
             <Route path="/" exact={true}>
