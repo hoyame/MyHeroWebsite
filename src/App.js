d881ddb76d5a8e1deeeafd3b091d421ecced050f
hoyame <zou.hoyame@gmail.com>
2021-02-17 13:21:52 +0100

    app: add header

--- src/App.js
+++ src/App.js
@@ -11,24 +11,41 @@ import HomeViews from './views/home';
 import './App.css';
 
 
+{
+  /*
+        <img className="logo" src="https://cdn.discordapp.com/attachments/650778484523794456/789992516505305098/Sans_titre_-_1.jpg"></img>
+        <p className="text-def copyright">© 2020-2021 MyHeroes. Tous droits réservés. </p>
+  */
+}
+
+
 const App = () => {
   return (
     <div className="App">
-        <img className="logo" src="https://cdn.discordapp.com/attachments/650778484523794456/789992516505305098/Sans_titre_-_1.jpg"></img>
-        <p className="text-def copyright">© 2020-2021 MyHeroes. Tous droits réservés. </p>
-        <Router>
-          <Switch>
-            <Route path="/" exact={true}>
-              <HomeViews />
-            </Route>
+        <div className="header">
+          <img className="logo" src="https://cdn.discordapp.com/attachments/650778484523794456/789992516505305098/Sans_titre_-_1.jpg"></img>
 
-            <Route 
-              path="/password-reset/:token"   
-              render={props => <PasswordView {...props} />}
-            >
-            </Route>
-          </Switch>
-        </Router>
+          <div className="header-buttons">
+            <div className="header-button">C.G.U</div>
+            <div className="header-button">Telecharger</div>
+          </div>
+        </div>
+
+        <div className="content">        
+          <Router>
+            <Switch>
+              <Route path="/" exact={true}>
+                <HomeViews />
+              </Route>
+
+              <Route 
+                path="/password-reset/:token"   
+                render={props => <PasswordView {...props} />}
+                >
+              </Route>
+            </Switch>
+          </Router>
+        </div>
     </div>
   );
 }
